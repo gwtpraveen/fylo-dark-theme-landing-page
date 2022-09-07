7cab2a52eee095bfaf83111eb66177b62e3d95d4
Praveen Thamindu <praveenthamindu@gmail.com>
2022-09-07 20:43:17 +0530

    configure tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+    colors: {
+      primary : "hsl(218, 28%, 13%)",
+      darkBlue : "hsl(217, 28%, 15%)",
+      footer : "hsl(216, 53%, 9%)",
+      testimonials : "hsl(219, 30%, 18%)",
+      cyan : "hsl(176, 68%, 64%)",
+      blue : "hsl(198, 60%, 50%)",
+      lightred : "hsl(0, 100%, 63%)",
+      white : "hsl(0, 0%, 100%)"
+    }
+  },
+  plugins: [],
+}
